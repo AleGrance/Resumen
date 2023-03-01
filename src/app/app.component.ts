299c5802e531d048a49bcebace5a1bd4b7571b34
alegrance <alegrance1000@gmail.com>
2023-02-28 22:49:33 -0300

    add age calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prueba';
+  currentYear = new Date().getFullYear();
+  currentMonth = new Date().getMonth();
+  age = 0;
+
+  ngOnInit() {
+    this.calcularEdad();
+    console.log(this.currentMonth + 1);
+  }
+
+  calcularEdad() {
+    let born = 1992;
+
+    if (this.currentMonth < 11) {
+      this.age = (this.currentYear - born) - 1;
+    } else {
+      this.age = (this.currentYear - born);
+    }
+  }
 }
